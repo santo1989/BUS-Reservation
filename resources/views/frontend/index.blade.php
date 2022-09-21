6012aa9d05574756fcd4cb142ac46a1ac071c189
santo1989 <65486217+santo1989@users.noreply.github.com>
2022-09-21 19:42:50 +0600

    frontend structure update santo commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,10 @@
+<x-frontend-layout>
+
+    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+        {{-- @foreach ($products as $product)
+            <x-frontend.product-card :product="$product"/>
+        @endforeach --}}
+
+    </div>
+</x-frontend-layout>
