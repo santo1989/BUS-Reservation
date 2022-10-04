941c539e5b4b6f115904366c5321584aec2ac7c5
Ismat-Farjana <ismat15-6535@diu.edu.bd>
2022-10-04 06:47:27 +0600

    index done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,6 @@
 <x-frontend.layouts.master>
-    
 
-   
+    <div style="text-align: center; padding-top:60px; padding-bottom:70px;">
+        <img src="{{ asset('ui/frontend/images/Phantom_Tranzit.jpg')}}" alt="" heigt=400px; width=400px; class="logo-image">
+    </div>
 </x-frontend.layouts.master>
