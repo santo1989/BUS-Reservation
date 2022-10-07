7e06155859e37747259b9533a3f8fa2ceb4981b5
santo1989 <65486217+santo1989@users.noreply.github.com>
2022-10-07 20:48:48 +0600

    update frontend

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,7 +46,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role_id' => 4,
+            'role_id' => 3,
 
         ]);
 
